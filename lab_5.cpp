64369f4cdba4ac5ec5149696dd96b5dba2f8542e
Alex4021 <Alex4021@users.noreply.github.com>
2019-12-12 22:31:16 +0300

    Добавьте файлы проекта.

---
+++ lab_5.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int x, n, k;
+	double z, y , s, f;
+	cout << "Enter z,y,N" << endl;
+	cin >> z >> y >> n;
+	s = 0;
+	k = 0;
+	for (x = 1; x <= n, x++)
+	{
+		f = pow(x, 2) / z - pow(z, 3) / pow(x + y, 2) + pow(x, 2) * pow(2 - y, 2) / pow(x + 2, 2);
+		cout << "F=" << f << endl;
+		if (x % 2 == 1)
+		{
+			s = s + f;
+			k += 1;
+		}
+	}
+	cout << "Количество нечетных членов: " << k << endl;
+	cout << "Сумма нечетных членов: " << s << endl;
+	system("pause");
+	return 0;
+}
